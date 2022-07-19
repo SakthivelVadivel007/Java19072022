3e30a8455f5a047385480becad17af02d4d0ecf8
SakthivelVadivel007 <sakthivel.v205@gmail.com>
2022-07-19 22:53:12 +0530

    My task

---
+++ src/org/com/Git.java
@@ -0,0 +1,17 @@
+package org.com;
+
+
+
+public class Git {
+
+	private void bike() {
+		System.out.println("kavaski");
+			}
+		public static void main(String[] args) {
+			Git sa = new Git();
+			sa.bike();
+					
+		}
+		}
+
+
